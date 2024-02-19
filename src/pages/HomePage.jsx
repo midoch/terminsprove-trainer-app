7d7d4fc89b56a36299d225b7f5cb1c77f4b246ff
mido <101448145+midoch@users.noreply.github.com>
2024-02-19 21:42:20 +0100

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,40 +1,23 @@
 import { Link } from "react-router-dom";
-import WelcomeBackground from "../assets/welcome-background.jpg";
-import WelcomeCenter from "../assets/welcome-center.jpg";
+import Activities from "../components/Activities";
+import BurgerMenu from "../components/BurgerMenu";
 
-const Home = () => {
+const HomePage = () => {
   return (
-    <div className="flex flex-col h-screen relative">
-      <div className="flex-1">
-        <img
-          className="object-cover h-full w-full"
-          src={WelcomeBackground}
-          alt="Welcome Background"
-        />
-
-        <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 mt-10 mr-4 text-left">
-          <h1 className="text-6xl font-bold text-[#F1C40E]">
-            Believe Yourself
-          </h1>
-          <p className="text-lg mt-4 font-bold text-white">Train like a pro</p>
+    <main className="">
+      <BurgerMenu />
+      <div className="">
+        <h3 className="">Popular classes</h3>
+      </div>
+      <div className="">
+        <Activities />
+        <h4 className="">Classes for you</h4>
+        <div className="">
+          <Activities />
         </div>
       </div>
-
-      <div className="flex-1">
-        <img
-          className="object-cover h-full w-full"
-          src={WelcomeCenter}
-          alt="Welcome Center"
-        />
-        <Link
-          to="/Login"
-          className="absolute bottom-12 left-24 bg-[#F1C40E] text-black font-semibold py-4 px-8 rounded-3xl"
-        >
-          START TRAINING
-        </Link>
-      </div>
-    </div>
+    </main>
   );
 };
 
-export default Home;
+export default HomePage;
