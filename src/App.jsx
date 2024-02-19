3dbc200e5f100556d87c461148247c1e06ca9118
mido <101448145+midoch@users.noreply.github.com>
2024-02-19 21:42:40 +0100

    fixed a wrong path

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import HomePage from "./pages/HomePage";
+import WelcomePage from "./pages/WelcomePage";
 
 function App() {
   return (
     <main className="min-h-screen">
-      <HomePage />
+      <WelcomePage />
     </main>
   );
 }
