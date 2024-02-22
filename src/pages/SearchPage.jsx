1f2ad2c954ede72cbfa6858e2c438a3dbe690345
mido <101448145+midoch@users.noreply.github.com>
2024-02-22 18:02:14 +0100

    Added search filter and comments

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -9,43 +9,70 @@ const SearchPage = () => {
   const [popularTrainers, setPopularTrainers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
 
   useEffect(() => {
+    // Function to fetch activities and trainers data
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:4000/api/v1/classes");
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
+        // Fetch activities data
+        const activitiesResponse = await fetch(
+          "http://localhost:4000/api/v1/classes"
+        );
+        if (!activitiesResponse.ok) {
+          throw new Error("Failed to fetch activities data");
         }
-        const data = await response.json();
-        setActivities(data);
+        const activitiesData = await activitiesResponse.json();
+        setActivities(activitiesData);
+
+        // Fetch popular trainers data
+        const trainersResponse = await fetch(
+          "http://localhost:4000/api/v1/trainers"
+        );
+        if (!trainersResponse.ok) {
+          throw new Error("Failed to fetch trainers data");
+        }
+        const trainersData = await trainersResponse.json();
+        setPopularTrainers(trainersData.slice(0, 4));
+
+        // Set loading to false after fetching data successfully
         setLoading(false);
       } catch (error) {
+        // Set error message if data fetching fails
         setError(error.message);
         setLoading(false);
       }
     };
 
-    fetchData();
-
-    fetch("http://localhost:4000/api/v1/trainers")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to fetch trainers data");
-        }
-        return response.json();
-      })
-      .then((data) => setPopularTrainers(data.slice(0, 4)))
-      .catch((error) => {
-        setError(error.message);
-        setLoading(false);
-      });
+    fetchData(); // Call fetchData function when component mounts
   }, []);
 
+  // Function to handle search query change
+  const handleSearchChange = (event) => {
+    const { value } = event.target;
+    setSearchQuery(value); // Update searchQuery state with the entered value
+  };
+
+  // Filter activities based on search query
+  const filteredActivities = searchQuery
+    ? activities.filter((activity) =>
+        activity.className.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    : activities;
+
+  // Filter trainers based on search query
+  const filteredTrainers = searchQuery
+    ? popularTrainers.filter((trainer) =>
+        trainer.trainerName.toLowerCase().includes(searchQuery.toLowerCase())
+      )
+    : popularTrainers;
+
+  // Display loading spinner while data is being fetched
   if (loading) {
     return <Loading />;
   }
 
+  // Display error message if data fetching fails
   if (error) {
     return <ErrorPage />;
   }
@@ -53,12 +80,21 @@ const SearchPage = () => {
   return (
     <div className="flex flex-col p-4">
       <Navigation />
-      <div className="flex justify-between p-2 my-6">
-        <h1 className="text-2xl font-semibold">Search</h1>
+      <div className="p-2 my-2">
+        <h1 className="text-2xl font-semibold mb-4">Search</h1>
+        {/* Input field for search */}
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          placeholder="Search classes"
+          className="block w-full px-4 py-2 border rounded-3xl focus:outline-none focus:border-black"
+        />
       </div>
       <h2 className="text-xl font-bold mt-6">Popular Classes</h2>
+      {/* Display filtered activities */}
       <div className="flex overflow-x-auto mt-2 mb-8 no-scrollbar">
-        {activities.map((classItem) => (
+        {filteredActivities.map((classItem) => (
           <Link key={classItem.id} to={`/search`} className="m-2 relative">
             <img
               src={classItem.asset.url}
@@ -73,8 +109,9 @@ const SearchPage = () => {
       </div>
 
       <h2 className="text-xl font-bold">Popular Trainers</h2>
+      {/* Display filtered trainers */}
       <div className="flex flex-col mt-2">
-        {popularTrainers.map((trainer) => (
+        {filteredTrainers.map((trainer) => (
           <Link
             key={trainer.id}
             to={`/search`}
